800abceeac2909abfc09a0e98e15626685bfd9cb
dave900000 <mthampton@gmail.com>
2022-03-27 16:56:56 -0400

    Update phi.asm
    Added feature that produces a linked list of prime numbers as a return value - the address returned includes a header to the list ( see documentation in the code ).

--- phi.asm
+++ phi.asm
@@ -6,8 +6,14 @@
 #
 # User supplies 1 integer as input, function will calculate and return the totient(phi).
 .data
+primelist: 	.space	12 #This is set aside as a linked list header to store the
+		           #prime numbers counted in this funciton.
 inputA:		.asciiz "Please enter a positive integer: "
 outputA: 	.asciiz "The totient is: "
+outputB:	.asciiz "The list of prime #'s: "
+newline:	.asciiz "\n"
+
+
 
 .text
 #Begin main function----------------------------------
@@ -26,7 +32,27 @@ outputA: 	.asciiz "The totient is: "
 	li $v0,1
 	move $a0,$s0
 	syscall
-	
+	li $v0,4
+	la $a0,newline
+	syscall
+	#Print out the list of prime #'s
+	li $v0,4
+	la $a0,outputB
+	syscall
+	la $t0,primelist
+	lw $t1,4($t0)
+	lw $t2,4($t1)
+printloop:
+	beq $t2,$zero,end
+	li $v0,1
+	lw $a0,0($t2)
+	syscall
+	lw $t2,4($t2)
+	li $v0,4
+	la $a0,newline
+	syscall
+	j printloop
+		
 end:
 	li $v0,10
 	syscall
@@ -37,12 +63,20 @@ end:
 #Begin phi function----------------------------------
 #Input: $a0 contains positive integer to calculate from.
 #Output: $v0 will contain the totient(phi) value.
+#$v1 will contain the address of a linked list header
+#that points to a list of the primes generated from
+#this function.
 #
 phi:
 	#Push params and return value to stack
 	addi $sp,$sp -8
 	sw $ra,4($sp)
 	sw $a0,0($sp)
+	
+	#Add '1' to the primelist ( 1 is alwayse prime ).
+	li $a0,1
+	jal storeprime
+	lw $a0,0($sp)
 	
 	#Local vars needed
 	li $t0,1 #Will contain value of the result
@@ -58,6 +92,8 @@ philoop:
 	
 	#Save local variables to stack
 	#Call gcd function then reload vars.
+	#Call storeprime to add prime # to primelist
+	#Restore $a0
 	addi $sp,$sp,-12
 	sw $t0,8($sp)
 	sw $t1,4($sp)
@@ -67,8 +103,21 @@ philoop:
 	lw $t1,4($sp)
 	lw $t2,0($sp)
 	addi $sp,$sp,12
-	
+
 	bne $v0,$t2,nextphiloop
+	#number is prime - save it to primelist then
+	#increment $t0.
+	move $a0,$t1
+	addi $sp,$sp,-12
+	sw $t0,8($sp)
+	sw $t1,4($sp)
+	sw $t2,0($sp)
+	jal storeprime
+	lw $t0,8($sp)
+	lw $t1,4($sp)
+	lw $t2,0($sp)
+	addi $sp,$sp,12
+	lw $a0,0($sp)
 	addi $t0,$t0,1
 nextphiloop:
 	addi $t1,$t1,1
@@ -120,7 +169,62 @@ gcdret:
 	
 	jr $ra
 #
-#End gcd function------------------------------------- 
+#End gcd function-------------------------------------
+
+#Begin storeprime function----------------------------
+#Input: $a0 will contain an integer to add to a dynamic list
+#Requires a 12-byte item in .data named: primelist
+#Output: None. Creates a new node in the list and adds the data
+#then updates the tail pointer. If the list was empty, head and
+#tail pointers are set to the new node.
+#Size variable in the header will be incremented.
+#STRUCTURE of 12-byte header 'primelist'
+#
+#	BYTE OFFSET	CONTENTS
+#	0		list size int
+#	4		address to head of list
+#	8		address to tail of list
+#
+storeprime:
+	#Stack operatoins
+	addi $sp,$sp,-8
+	sw $ra,4($sp)
+	sw $a0,0($sp)	
+	#Create new node
+	li $v0,9
+	li $a0,8
+	syscall
+	move $t0,$v0
+	lw $a0,0($sp)
+	sw $a0,0($t0)
+	#Get header and check if list is empty
+	la $t1,primelist
+	lw $t2,0($t1)
+	bne $t2,$zero,skipempty
+	#List is empty, point head and tail
+	#addresses in the header to the new node
+	sw $t0,4($t1)
+	sw $t0,8($t1)
+	j endstoreprime
+skipempty:
+	#List is not empty. Update tail node next
+	#address to point to new node, then set
+	#tail address itself to the address of the
+	#new node.
+	lw $t3,8($t1)
+	sw $t0,4($t3)
+	move $t3,$t0
+	sw $t3,8($t1)
+endstoreprime:
+	#Increment size of list then return.
+	addi $t2,$t2,1
+	sw $t2,0($t1)
+	lw $ra,4($sp)
+	lw $a0,0($sp)
+	addi $sp,$sp,8
+	jr $ra
+#
+#End storeprime function---------------------------	 
 	
 	
 	
