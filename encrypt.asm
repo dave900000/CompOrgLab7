add3b7bacd42b5f271a6802960dedbf7656fcc67
dave900000 <mthampton@gmail.com>
2022-05-01 21:31:39 -0400

    Update encrypt.asm
    Overhaul - works with 1 char

--- encrypt.asm
+++ encrypt.asm
@@ -9,9 +9,9 @@
 #
 .data
 
-input_number_array: .space 300
-message: .byte 200
-encrypted_word: .word 200
+message: .space 200
+encrypted_char: .space 200
+input_number_array: .space 200
 newline: .ascii "\n"
 str_file: .asciiz "encrypted.txt"
 str_exponent: .asciiz "\n enter your exponent:\n"
@@ -20,187 +20,128 @@ str_plain_text: .asciiz "enter plain text: \n"
 end_message: .asciiz "enter plain text: \n"
 
 
-.align 2
-
 .text
-.globl __start	# leave this here for the moment
 
-
-
-__start:
 main:
-	li $v0, 4              	# system call code for Print String
-	la $a0, str_exponent    # Print "get expo" string        
+	li $v0, 4              		# system call code for Print String
+	la $a0, str_exponent    	# Print "get expo" string        
 	syscall  
 	
-	li $v0, 5       	# system call code for Get String
+	li $v0, 5       		# system call code for Get String
 	syscall	
-	move $s4, $v0		# Get exponent (e) and move to $s4
+	move $s0, $v0			# Get exponent (e) and move to $s0
 	
-	li $v0, 4		# system call code for Print String
-	la $a0, str_modulus     # Print "get mod" string       
+	li $v0, 4			# system call code for Print String
+	la $a0, str_modulus     	# Print "get mod" string       
 	syscall  
 
-	li $v0, 5       	# system call code for Get String
+	li $v0, 5       		# system call code for Get String
 	syscall			
-	move $s5, $v0		# Get modulus (n) move to $s5
+	move $s1, $v0			# Get modulus (n) move to $s1
 	
-	li $v0, 4              	# system call code for Print String
-	la $a0, str_plain_text  # Print "Enter string"           
+	li $v0, 4              		# system call code for Print String
+	la $a0, str_plain_text  	# Print "Enter string"           
 	syscall  
 	
-	li $v0, 8       	# Take in input
-    	la $a0, message  	# Load byte space into address
-   	addi $a1, $a1, 200      # Allot the byte space for string
-  	move $s7, $a0   	# Save string to $s7
+	li $v0, 8       		# Take in input
+    	la $a0, message  		# Load byte space into address
+   	li $a1, 200      		# Allot the byte space for string
+  	move $s7, $a0   		# Save string to $s7
  	syscall
-		  
-	add $s6, $zero, $zero	# Save 0 to $s6
-	
-	loop: 	
-		add $t0, $s7, $s6 	# address of byte to examine next
-		lb $t1, 0($t0)		# load that byte to get *(s + ct)
-		beq $t1, $zero, exit 	# branch if *(s + ct) == ’\0’
-		addi $s6, $s6, 1 	# increment ct
-		j loop
+ 	
+ 	
+ 	li $t0, 0			# counter for loop
+ 	loop: 	
+		add $t1, $s7, $t0 		# increment $s7 (message) by the counter to access the next char 
+		lb $t2, 0($t1)			# load byte of addr into $t2
+		beq $t2, $zero, exit 		# branch if $t2 is 0x00 
+		addi $t0, $t0, 1 		# increment counter
+		move $s4, $t0			# move str_length to $s4 for later use
+		j loop			
 	exit:
-	addi $s6 ,$s6, -1		# set c back to legnth of s
-	la $s0, input_number_array 	# unsigned int* ptr = number_array
-	li $s1, 1			# num_array_length
+		addi $s4 ,$s4, -1		# set c back to legnth of s
+			
+	jal encrypt
 	
-	encrypt_loop_test_in: 		# do {
-		#li $v0, 5 
-		#syscall
-		lb $t0, 0($s7)  	#    scanf("%d", &read_num)
-		sw $t0, 0($s0)		#    *ptr = read_num
-		addi $s1, $s1, 1	#    num_array_length++
-		addi $s0, $s0, 4
-		addi $s7, $s7, 1	#    ptr += sizeof(unsigned int)
-		bge $s6, $s1, encrypt_loop_test_in 	# } while(read_num != 0)
-
-		la $a0, input_number_array
-		move $a1, $s4
-		move $a2, $s5
-		move $a3, $s1
-
-		jal encrypt
-
-		move $s2, $s6
-		move $s1, $v0
-
 	# Open (for reading) a file
-        li $v0, 13
-        la $a0, str_file
-        li $a1, 1
-        li $a2, 0
-	syscall  		# File descriptor gets returned in $v0
-	move $s6, $v0  		# Syscall 15 requires file descriptor in $a0
-   	 
-   	# file_write: 
-          
-	# move $s5, $v0	#: byte output
-	la $s5, encrypted_word
-	sll $t0, $s2, 2
-        li $t1, 0
-        move $t2, $s5
-        
-        encrypt_loop_test_out:
-            
-        # address of buffer from which to write
-        # lw $t1 ,0($s1)
-       		
-		lb $t3, ($s1)
-       		sb $t3, ($t2)
-       		addi $t1, $t1, 1
-		addi $s1 , $s1, 1
-		addi $t2 , $t2, 1
-        	bne $t0, $t1 , encrypt_loop_test_out
-            	li $v0, 15
-          	move $a0, $s6 		# file descriptor 
-          	move $a1, $s5 
-		# li $a2, 16
-		addi $t0, $t0 , 4
-		move   $a2, $t0         # hardcoded buffer length
-            	syscall
-        	# bgt $s2, $zero, encrypt_loop_test_out
-        
-    	# file_close:
-        li $v0, 16  		# $a0 already has the file descriptor
-      	move $a0, $s6      
-        syscall
-        
-  	li $v0, 10    		# syscall code 10 for terminating the program
-  	syscall
-
-	powmod:
-	# PUSH_REGISTERS
-	li $t0, 1		# unsigned int x = 1
-	move $t1, $a0		# unsigned int y = a
-
-	powmod_loop:					# while (
-		ble $a1, $zero, powmod_while_end 	# b > 0 ) {
-		
-		andi $t2, $a1, 1			# t2 = b % 2
-		beq $t2, $zero, powmod_b_mod_2		# if (t2) {
-		mul $t0, $t0, $t1			# x *= y
-		divu $t0, $a2				# HI = x % mod
-		mfhi $t0				# x = HI
-		
-		powmod_b_mod_2:				# }
-		mul $t1, $t1, $t1			# y *= y
-		divu $t1, $a2				# HI = y % mod
-		mfhi $t1				# y = HI
-
-		srl $a1, $a1, 1				# b /= 2
-
-		j powmod_loop
-		
-		powmod_while_end:
-
-		move $v0, $t0
-		# POP_REGISTERS
-		jr $ra 			# return a
-		# END
-
+        li $v0, 13				# syscall for open file
+        la $a0, str_file			# name of file
+        li $a1, 1				# open for writing 
+        li $a2, 0				# mode is ignored 
+	syscall  				# File descriptor gets returned in $v0
+	move $s6, $v0  				# Syscall 15 requires file descriptor in $a0
+	
+	# Write to the file: 
+	li $v0, 15				# syscall for write file
+	move $a0, $s6				# file descriptor
+	la $a1, encrypted_char			# addr of encrypted message
+	li $a2, 200				# hardcoded length of message
+        syscall					# write to file
+	
+	# Close the file
+	li $v0, 16				# syscall for close file
+	move $a0, $s6				# file descriptor to close
+	syscall					# close file
+	
+	###############PRINT TEST for encrypted chars (IRGNORE)#####################
+	#li $t0, 0				# counter for print_loop
+	#la $t1, encrypted_char			# load addr of first encrypted_char
+	#print_loop:
+	#	bge $t0, $s4, end		# branch if >= length of the message
+	#	add $t2, $t1, $t0		# add counter to addr of char
+	#	lb $s7, 0($t2)			# load byte into $t3
+	#	
+		li $v0, 4              		# system call code for Print String
+	#	move $a0, $s7    		# move content of $t3 into $a0 for printing        
+		la $a0, encrypted_char
+		syscall
+	#	addi $t0, $t0, 1
+	#	j print_loop  
+	
+	end:
+		li $v0, 10    			# syscall for terminating the program
+  		syscall				# end program
+	
 
 encrypt:
-	# PUSH_REGISTERS
-	move $t7, $ra
-	move $s0, $a0
-	move $s1, $a1
-	move $s2, $a2
-	move $s3, $a3
-
-	sll $a0, $s3, 2 			# size_in_bytes = text_length * 4
-	li $v0, 9 				# cipher = malloc(size_in_bytes)
-	syscall					# // output in $v0
-	move $s5, $v0				# cipher
-
-	li $s4, 0				# for (int i = 0; i < count; ++i) {
+	move $s6, $ra
+	li $t0, 0
+	move $t5, $s4				# ideally is equal to the length of the message
 	encrypt_loop:
-		bge $s4, $s3, encrypt_exit 	# 
-		sll $t0, $s4, 2
-		add $t0, $t0, $s0		# unsigned* msg = M + i;
-		lw $a0, 0($t0) 			# unsigned b = *msg;
+		bge $t0, $t5, encrypt_exit 	# branch if counter is >= length of pt
+		add $t2, $s7, $t0
+		lb $t1,0($t2)			# load byte from pt message into $t1
+		move $a0, $t1			# move pt char into $a0 for powmod
+		jal powmod			# jump/link to powmod for encryption 
+		la $s5, encrypted_char		#0x10010001
+		add $t8, $s5, $t0
+		sb $s2, 0($s5)			# store result of powmod ($s2) in $s4 
+		#addi $s7, $s7, 1		# add 1 to the address of $s7 to access the next char
+		addi $t0, $t0, 1		# add 1 to the address of $t0 to access the next byte in the string
+		
+		j encrypt_loop			
 
-		move $a1, $s1
-		move $a2, $s2
-		#move $s6, $s0
-		#move $s7, $s1
-		jal powmod			# unsigned mod (aka. $v0) = powmod(b, e, modN)
-		#move $s0, $s6
-		#move $s1, $s7
-		sll $t0, $s4, 2
-		add $t0, $t0, $s5		# unsigned* decry = cipher + i;
-		sw $v0, 0($t0)			# *decry = mod
-
-		addi $s4, $s4, 1		# ++i
-		j encrypt_loop			# } 
-	
+		
 	encrypt_exit:
-		move $v0, $s5
+		move $ra, $s6
+		jr $ra				# return to main
 
-	#POP_REGISTERS
-	jr $t7
 
-#END}}
+powmod:
+	li $t9, 1 				# start counter at 1 since the first expo is itself
+	move $t1, $a0				# load byte from pt string argument
+	move $t2, $s0				# move (e) from $s4 into $t2
+	move $t4, $s1				# move (n) from $s5 into $t4
+	move $t3, $t1				# set $t3 as the result/intial var for mult
+	expo:
+		bge $t9, $t2, mod		# branch to modulus part if counter is equal to exponent
+		mult $t3, $t1			# multiply (pt char) by itself (exponential mult)
+		mflo $t3			# take the result and store in $t3
+		addi $t9, $t9, 1		# add 1 to counter 
+		j expo				# jump back to top
+	mod:
+		div $t3, $t4			# divid the result of t^e by n (taking the remainder)
+		mfhi $s2			# store remainder in $s0
+		jr $ra 				# return to encrypt
+
+	
