8c97335e1249d1cc3b3930adf58a44d9a361fe26
dave900000 <mthampton@gmail.com>
2022-05-02 12:58:08 -0400

    Update decrypt.asm
    Rewrote and added powmodB function using modular exponentiation instead of floating point. Tested ok for single values.

--- decrypt.asm
+++ decrypt.asm
@@ -31,90 +31,46 @@ input_C:	.asciiz "Please enter the private key: "
 		li $v0,5
 		syscall
 		move $t2,$v0 #d into $t2
-		move $a0,$t1
+		move $a0,$t0
 		move $a1,$t2
-		move $a2,$t0
+		move $a2,$t1
 		
-		jal decrypt_str
+		jal powmodB
 
 end_main:
 		li $v0,10
 		syscall
 #End main-----------------------------------
 
-#Begin decrypt_str-------------------------
+#Begin powmodB------------------------------
 #
-#Input $a0 - modulus(n) $a1 - Private Key(d) $a2 - Encrypted byte value(c)
+#Input: $a0 contains encrypted or unencrypted byte value(c) or (m) 
+#	$a1 has exponent (d or e)
+#	$a2 has modulus (n)
+#Output: $v0 will contain c^d % n or m^e%n for encryption and decryption.
 #
-#Output: $v0 contains decrpyted byte value.
-decrypt_str:
-		#Push ra and params
-		addi $sp,$sp,-16
-		sw $ra,12($sp)
-		sw $a2,8($sp)
-		sw $a1,4($sp)
-		sw $a0,0($sp)
+powmodB:
+		addi $sp,$sp,-4
+		sw $ra,0($sp)
 		
-		move $a0,$a2
-		#a1 already has private key(d)
-		jal powFP #Get c^d and store in $t0
-		lw $a0,0($sp) #Reload modulus into $a0
-		mov.d $f12,$f0 #Move result of c^d into $f12
-		mtc1 $a0,$f14 #Move modulus into $f14
-		cvt.d.w $f14,$f14
-		jal modFP #This should return (c^d) % n int0 $v0
+		move $t0,$zero 	#Loop counter
+		move $t1,$zero 	#Initialize intermediate register to hold multiplcation results
+		li $v0,1	#Initialize result register to 1
 		
-		#Pop ra and params and return
-		lw $ra,12($sp)
-		lw $a2,8($sp)
-		lw $a1,4($sp)
-		lw $a0,0($sp)
-		addi $sp,$sp,16
+powmodB_loop:	
+		#Loop executes the calculation result = (bytevalue * lastresult) % n
+		#This is using the concept that (a ? b) mod m = [(a mod m) ? (b mod m)] mod m
+		#Result will be in $v0 when complete
+		addi $t0,$t0,1 		#Increment counter
+		multu $a0,$v0 		#Multiply byte value by last modulus calculation in $v0
+		mflo $t1
+		divu $t1,$a2		#Calculate modulus of above result and update $v0
+		mfhi $v0
+		blt $t0,$a1,powmodB_loop#Loop if counter < exponent
 		
-		
+		lw $ra,0($sp)
+		addi $sp,$sp,4
 		
 		jr $ra
 #
-#End decrypt_str----------------------------
-
-#Begin powFP function------------------------------
-# compute $a0 ^ $a1 and return in $F0
-powFP: 
-#Move and convert arguments to FP registers.
-mtc1 $a0,$f2
-cvt.d.w $f12,$f2 #$a0 converted to FP and stored in $f12
-#a1 needs no conversion as it is only used as an integer counter.
-
-li $v0,1
-mtc1 $v0,$f2
-cvt.d.w $f0,$f2 #Put value of 1 into $f0 ( return value ). As FP double.
-move $t1,$a1
-loop :
-beq $t1,$zero, exit
-mul.d $f0,$f0,$f12 #loop for the value of s2
-subi $t1,$t1,1
-j loop
-exit:
-jr $ra
-#end powFP-------------------------------------
-
-#Begin modFP function--------------------------
-#
-#In: $f12 contains number to mod  $f14 contains divisor (Values must be positive)
-#Out: $v0 will contain the integer result
-modFP:
-		mov.d $f0,$f12 #Mod will be caluclated into $f0 with repeated subtraction
-		round.w.d $f0,$f0
-
-loopModFP:	
-		c.lt.d $f0,$f14
-		bc1t exitLoopModFP #Exit when $f0 < $f14
-		sub.d $f0,$f0,$f14
-		j loopModFP  
-exitLoopModFP:
-		cvt.w.d $f0,$f0
-		mfc1 $v0,$f0
-		
-		jr $ra
-#
-#End modFP function----------------------------
+#End powmodB--------------------------------
