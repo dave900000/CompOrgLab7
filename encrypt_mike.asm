88a20ae2e6f495c94db937c771392a1a5c01e872
dave900000 <mthampton@gmail.com>
2022-05-02 21:56:24 -0400

    Update encrypt_mike.asm

--- encrypt_mike.asm
+++ encrypt_mike.asm
@@ -19,6 +19,12 @@ fout:   .asciiz "testout.txt"      # filename for output
 
 main:
 
+addi $sp, $sp, -16
+sw $t1, 0($sp)
+sw $t2, 4($sp)
+sw $t3, 8($sp)
+sw $t4, 12($sp)
+
          la $a0,str_plain_text #Load and print string asking for string
          li $v0,4
          syscall
@@ -29,8 +35,7 @@ main:
          move $t2,$a0 #save string to t0
          syscall
 	
- 	li $s0, 5   #e
- 	li $s1, 5    #mod
+
  	#la $t2, string # Load here the ttring  
 	li $t7, 0
 	
@@ -46,7 +51,7 @@ main:
        continue: 
        addi, $t7, $t7, 1
        move $a0, $t3	
-       jal powmod
+       jal powmodB
        
         move  $t3, $s3
 	sb $t3, 0($t2)
@@ -54,8 +59,9 @@ main:
 
         
         # Printing out the text
+        move  $a0, $v0
     	li $v0, 1
-    	move  $a0, $s3
+    	
     	syscall			
  
         add  $t2, $t2, 1   # Increment the address  
@@ -85,15 +91,67 @@ move $a0, $s6      # file descriptor to close
 syscall            # close file  
       
       
-      
-      
-      
-      
+ 	lw $t4 12($sp)
+	lw $t3 8($sp)
+	lw $t2 4($sp)
+	lw $t1 0($sp)
+	addi $sp,$sp,16     
        li $v0, 10  
        syscall  
-       
      
-    powmod:
+       
+#Begin powmodB------------------------------
+#
+#Input: $a0 contains encrypted or unencrypted byte value(c) or (m) 
+#	$a1 has exponent (d or e)
+#	$a2 has modulus (n)
+#Output: $v0 will contain c^d % n or m^e%n for encryption and decryption.
+#
+
+powmodB:
+		
+		li $a1, 35   #e
+ 		li $a2, 221    #mod
+ 		
+ 		
+		addi $sp, $sp, -8
+		sw $t0, 0($sp)
+		sw $t1, 4($sp)
+		
+		addi $sp,$sp,-4
+		sw $ra,0($sp)
+		
+		move $t0,$zero 	#Loop counter
+		move $t1,$zero 	#Initialize intermediate register to hold multiplcation results
+		li $v0,1	#Initialize result register to 1
+		
+powmodB_loop:	
+		#Loop executes the calculation result = (bytevalue * lastresult) % n
+		#This is using the concept that (a ? b) mod m = [(a mod m) ? (b mod m)] mod m
+		#Result will be in $v0 when complete
+		addi $t0,$t0,1 		#Increment counter
+		multu $a0,$v0 		#Multiply byte value by last modulus calculation in $v0
+		mflo $t1
+		divu $t1,$a2		#Calculate modulus of above result and update $v0
+		mfhi $v0
+		blt $t0,$a1,powmodB_loop#Loop if counter < exponent
+		
+		lw $ra,0($sp)
+		addi $sp,$sp,4
+		
+		lw $t1 4($sp)
+		lw $t0 0($sp)
+		addi $sp,$sp,8
+		
+		jr $ra
+#
+#End powmodB--------------------------------
+        
+                     
+                       
+                           
+     
+    powmod_old:
     addi $sp, $sp, -16
 sw $t1, 0($sp)
 sw $t2, 4($sp)
@@ -101,8 +159,8 @@ sw $t3, 8($sp)
 sw $t4, 12($sp)
 
 
-    	li $s0, 5   #e
- 	li $s1, 5    #mod
+
+    	
 	li $t9, 1 				# start counter at 1 since the first expo is itself
 	move $t1, $a0				# char
 	li $t2, 1				# set $t3 as the result/intial var for mult
